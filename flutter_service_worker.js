383b1f19a755ccabc79ba8a3cbc96bd54e0d1262
Gnanadurai G <103520908+Gnanaduraig@users.noreply.github.com>
2025-07-23 19:00:23 +0530

    new

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "48ee1c710306e2a41ecfa7360bea0188",
+const RESOURCES = {"flutter_bootstrap.js": "1fd4bf66694fb2ca75989b1da5b4fdeb",
 "version.json": "3955b1186f01b2d267f57a075aa99a74",
 "index.html": "3964d1781a9b9237fe34ffc90d10bc6d",
 "/": "3964d1781a9b9237fe34ffc90d10bc6d",
-"main.dart.js": "e94258e3c7194a2fa19487759c6e716b",
+"main.dart.js": "8a2293b51e5b86c44395cc1192901efe",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "a31f2bef2437c986c397c89ee66ad32a",
 "icons/Icon-192.png": "a31f2bef2437c986c397c89ee66ad32a",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "48ee1c710306e2a41ecfa7360bea0188",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "f5900d640b12e69ce0e67da345bfb6c1",
-"assets/fonts/MaterialIcons-Regular.otf": "3606d9dfb606d89e3c21610eff07e1d5",
+"assets/fonts/MaterialIcons-Regular.otf": "459035ce9bf4c211d65963814d750a87",
 "assets/assets/images/bible_logo.png": "d5ea1ca831e33a157eab73a98bb4f90d",
 "assets/assets/images/ask.png": "bdc431d7cc274ca181ab5ef470733637",
 "assets/assets/images/leaderboard.png": "3b10515a06860cabecb9fef62d10956c",
